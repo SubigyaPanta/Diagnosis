40e64c1b1ca11732487b3913e861a3f92da9c220
Subigya <subigyapanta@gmail.com>
2017-12-15 12:21:04 +0700

    minimal summary

--- main_with_summary.py
+++ main_with_summary.py
@@ -12,7 +12,7 @@ from tensorflow.python import debug as tf_debug
 ROOT_FOLDER = os.path.dirname(os.path.realpath(__file__))
 DATA_FOLDER = ROOT_FOLDER + '/data/'
 ALL_DATA = 'input/breast-cancer-wisconsin.data'
-SUMMARY_FOLDER = ROOT_FOLDER + 'summary'
+SUMMARY_FOLDER = ROOT_FOLDER + '/summary/'
 
 def main():
     # Import data
@@ -31,12 +31,14 @@ def main():
     # Parameters
     learning_rate = 0.05
     batch_size = 100 # what is this ? => How many data should we use at once
-    training_epoch = 1500 # what is this ? => How many times should we train the model with complete training data
-    display_step = 1 # what is this ?
+    training_epoch = 500 # what is this ? => How many times should we train the model with complete training data
+    display_step = 1 # what is this ? => training interval to show output on console as a progress.
     label_values = {2: np.array([1, 0]), 4: np.array([0, 1])}
+
     # tf Graph input
-    x = tf.placeholder(tf.float32, [None, 9], 'x_input') # there are 9 features, the first one is id which is not a feature
-    y = tf.placeholder(tf.float32, [None, 2], 'y_input')
+    with tf.name_scope('input'):
+        x = tf.placeholder(tf.float32, [None, 9], 'x_input') # there are 9 features, the first one is id which is not a feature
+        y = tf.placeholder(tf.float32, [None, 2], 'y_input')
 
     # Set Model weights
     W = tf.Variable(tf.random_normal([9,2])) # 9 rows and 2 columns
@@ -57,7 +59,14 @@ def main():
     # Start training
     with tf.Session() as sess:
         # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+
+        # # For Graph summaries
+        # graph = tf.Graph()
+        # with graph.as_default():
+
         sess.run(init)
+
+        train_writer = tf.summary.FileWriter(SUMMARY_FOLDER + 'train', graph=sess.graph)
 
         # Training cycle
         for epoch in range(training_epoch):
@@ -65,9 +74,14 @@ def main():
                                                           y: get_label_values_from_label(train_label, label_values)})
             if (epoch+1) % display_step == 0:
                 print('Epoch: ', '%04d' % (epoch + 1), 'Cost: ', '{:.9f}'.format(c))
+            tf.summary.scalar('Cost', c)
+
 
         print('Optimization Finished!')
         print('W=', W.eval(), 'b=', b.eval())
+        tf.summary.merge_all()
+        # train_writer.add_summary(summary={'cost': c})
+        train_writer.flush()
 
         # Calculate accuracy tf.equal Returns the truth value of (x == y) element-wise.
         # print(y.name)
